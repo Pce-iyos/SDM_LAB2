19e1397e568e1addd6744d86cfb8d326f79269fb
Pce-iyos <61274755+Pce-iyos@users.noreply.github.com>
2024-06-10 03:41:40 +0200

    Update knowledge_instance.py

--- knowledge_instance.py
+++ knowledge_instance.py
@@ -1,10 +1,10 @@
+
 import urllib.parse    
 import csv
-from rdflib import Graph, Namespace, Literal, URIRef, RDF, RDFS, XSD
+from rdflib import Graph, Namespace, Literal, URIRef, RDF, XSD
 
 def create_abox(pub):
     g = Graph()
-    # pub = Namespace("http://example.com/")
     g.bind("pub", pub)
     
     # Load entities from CSV files and their properties
@@ -20,14 +20,14 @@ def create_abox(pub):
     }
 
     for filename, (class_uri, id_field, attributes) in entity_files.items():
-        with open(f'./DATA/CSV_files/{filename}', 'r', encoding='utf8') as csvfile:
+        with open(f'/home/pce/Documents/LAB1_SDM/DATA/CSV_files/{filename}', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                entity = URIRef(pub[urllib.parse.quote(row[id_field])])
-                g.add((entity, RDF.type, class_uri))
+                entity_uri = URIRef(f"{pub}{class_uri}/{urllib.parse.quote(row[id_field])}")
+                g.add((entity_uri, RDF.type, class_uri))
                 for attr in attributes:
                     if row.get(attr):
-                        g.add((entity,  URIRef(pub[urllib.parse.quote(attr)]), Literal(row[attr])))
+                        g.add((entity_uri,  URIRef(f"{pub}property/{urllib.parse.quote(attr)}"), Literal(row[attr])))
 
     # Relationship files configuration
     relationship_files = {
@@ -54,7 +54,6 @@ def create_abox(pub):
             'objects': (pub.Proceedings, 'END_ID'),
             'predicate': pub.paperPartOfwork
         },
-       
         'paper_presented_in_conference.csv': {
             'subjects': (pub.Paper, 'START_ID'),
             'objects': (pub.Conference, 'END_ID'),
@@ -88,7 +87,7 @@ def create_abox(pub):
     }
 
     for file_name, config in relationship_files.items():
-        with open(f'./DATA/CSV_files/{file_name}', 'r', newline='') as csvfile:
+        with open(f'/home/pce/Documents/LAB1_SDM/DATA/CSV_files/{file_name}', 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             subj_info = config['subjects']
             obj_info = config['objects']
@@ -96,33 +95,32 @@ def create_abox(pub):
             extras = config.get('extras', {})
             
             for row in reader:
-                subject =  URIRef(pub[urllib.parse.quote(row[subj_info[1]])])
-                object = URIRef(pub[urllib.parse.quote(row[obj_info[1]])])
+                subject = URIRef(f"{pub}{subj_info[0]}/{urllib.parse.quote(row[subj_info[1]])}")
+                object = URIRef(f"{pub}{obj_info[0]}/{urllib.parse.quote(row[obj_info[1]])}")
                 g.add((subject, predicate, object))
                 
                 for extra_key, (datatype, column) in extras.items():
                     if column in row and row[column]:  
-                        extra_predicate = URIRef(pub[urllib.parse.quote(extra_key)])
+                        extra_predicate = URIRef(f"{pub}property/{urllib.parse.quote(extra_key)}")
                         g.add((subject, extra_predicate, Literal(row[column], datatype=datatype)))
                         
-
     # Handling chair and editor assignments
-    with open('./DATA/CSV_files/chair_assign_reviewer.csv', 'r') as csvfile:
+    with open('/home/pce/Documents/LAB1_SDM/DATA/CSV_files/chair_assign_reviewer.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            chair_id = URIRef(pub[urllib.parse.quote(row['START_ID'])])
-            reviewer_id = URIRef(pub[urllib.parse.quote(row['END_ID'])])
+            chair_id = URIRef(f"{pub}Chair/{urllib.parse.quote(row['START_ID'])}")
+            reviewer_id = URIRef(f"{pub}Reviewer/{urllib.parse.quote(row['END_ID'])}")
            
             g.add((chair_id, RDF.type, pub.Chair))
             g.add((chair_id, pub.assignReviewer, reviewer_id))
             g.add((chair_id, pub.chair_name, Literal(row['conferenceChair'], datatype=XSD.string)))
             g.add((chair_id, pub.chair_review_policy, Literal(row['reviewPolicy'], datatype=XSD.integer)))
 
-    with open('./DATA/CSV_files/editor_assign_reviewer.csv', 'r') as csvfile:
+    with open('/home/pce/Documents/LAB1_SDM/DATA/CSV_files/editor_assign_reviewer.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            editor_id = URIRef(pub[urllib.parse.quote(row['START_ID'])])
-            reviewer_id = URIRef(pub[urllib.parse.quote(row['END_ID'])])
+            editor_id = URIRef(f"{pub}Editor/{urllib.parse.quote(row['START_ID'])}")
+            reviewer_id = URIRef(f"{pub}Reviewer/{urllib.parse.quote(row['END_ID'])}")
             
             g.add((editor_id, RDF.type, pub.Editor))
             g.add((editor_id, pub.assignReviewer, reviewer_id))
@@ -132,15 +130,14 @@ def create_abox(pub):
     return g
 
 def create_volume_instances(g, pub):
-    
     volume_dict = {}
     # Extract volume and year from journal.csv
-    with open('./DATA/CSV_files/journal.csv', 'r', encoding='utf8') as csvfile:
+    with open('/home/pce/Documents/LAB1_SDM/DATA/CSV_files/journal.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             volume_id = f"{row['publicationId']}_volume_{row['volume']}"
-            volume_uri = URIRef(pub[urllib.parse.quote(f"{row['publicationId']}_volume_{row['volume']}")])
-            journal_uri = URIRef(pub[urllib.parse.quote(row['publicationId'])])
+            volume_uri = URIRef(f"{pub}Volume/{urllib.parse.quote(volume_id)}")
+            journal_uri = URIRef(f"{pub}Journal/{urllib.parse.quote(row['publicationId'])}")
             
             # Create Volume instance and add properties
             g.add((volume_uri, RDF.type, pub.Volume))
@@ -150,23 +147,21 @@ def create_volume_instances(g, pub):
             # Establish relationship between Volume and Journal
             g.add((volume_uri, pub.publishedIn, journal_uri))
             
-            
             # Establish relationship between Volume and Publication
-            publication_uri = URIRef(pub[row['publicationId']])
+            publication_uri = URIRef(f"{pub}Publication/{urllib.parse.quote(row['publicationId'])}")
             g.add((volume_uri, pub.VolumeInPub, publication_uri))
             
-              # Store volume information for later use
+            # Store volume information for later use
             volume_dict[(row['publicationId'], row['volume'])] = volume_uri
 
-    return g , volume_dict
-
+    return g, volume_dict
 
 def add_event_types(g, pub):
     # Extract event types from proceedings.csv
-    with open('./DATA/CSV_files/proceedings.csv', 'r', encoding='utf8') as csvfile:
+    with open('/home/pce/Documents/LAB1_SDM/DATA/CSV_files/proceedings.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            proceeding_uri = URIRef(pub[urllib.parse.quote(row['ID'])])
+            proceeding_uri = URIRef(f"{pub}Proceedings/{urllib.parse.quote(row['ID'])}")
             g.add((proceeding_uri, RDF.type, pub.Proceedings))
             g.add((proceeding_uri, pub.name, Literal(row['name'])))
             g.add((proceeding_uri, pub.city, Literal(row['city'])))
@@ -179,10 +174,9 @@ def add_event_types(g, pub):
 
     return g
 
-
 def extract_paper_info():
     paper_info = {}
-    with open('./DATA/CSV_files/paper_published_in_journal.csv', 'r') as csvfile:
+    with open('/home/pce/Documents/LAB1_SDM/DATA/CSV_files/paper_published_in_journal.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             paper_id = row['START_ID']
@@ -193,15 +187,13 @@ def extract_paper_info():
 def link_papers_to_volumes(g, pub, paper_info, volume_dict):
     # Link paper instances to volume instances using paperPartOfJour property
     for paper_id, publication_id in paper_info.items():
-        paper_uri = URIRef(pub[urllib.parse.quote(paper_id)])
+        paper_uri = URIRef(f"{pub}Paper/{urllib.parse.quote(paper_id)}")
         # Find the corresponding volume in volume_dict
         for (pub_id, vol), volume_uri in volume_dict.items():
             if pub_id == publication_id:
                 g.add((paper_uri, pub.paperPartOfJour, volume_uri))
                 break
     return g
-
-
 
 def link_events_to_publications(g, pub):
     # Link event instances to publication instances using eventInPub property
@@ -211,11 +203,11 @@ def link_events_to_publications(g, pub):
     }
     
     for filename, class_uri in event_files.items():
-        with open(f'./DATA/CSV_files/{filename}', 'r') as csvfile:
+        with open(f'/home/pce/Documents/LAB1_SDM/DATA/CSV_files/{filename}', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                event_uri = URIRef(pub[urllib.parse.quote(row['publicationId'])])
-                publication_uri = URIRef(pub[row['publicationId']])
+                event_uri = URIRef(f"{pub}{class_uri}/{urllib.parse.quote(row['publicationId'])}")
+                publication_uri = URIRef(f"{pub}Publication/{urllib.parse.quote(row['publicationId'])}")
                 g.add((event_uri, pub.eventInPub, publication_uri))
 
     return g
@@ -228,13 +220,11 @@ abox_graph = add_event_types(abox_graph, pub)
 
 paper_info = extract_paper_info()
 
-
 # Link papers to volumes
 abox_graph = link_papers_to_volumes(abox_graph, pub, paper_info, volume_dict)
 
-
-# abox_graph = link_papers_to_volumes(abox_graph, Namespace("http://example.com/"), volume_dict)
+# Link events to publications
 abox_graph = link_events_to_publications(abox_graph, pub)
-abox_graph.serialize(destination='./DATA/abox_result.ttl', format='turtle')
-    
-    
+
+# Serialize the final graph to a file
+abox_graph.serialize(destination='/home/pce/Documents/LAB1_SDM/DATA/abox_result.ttl', format='turtle')
